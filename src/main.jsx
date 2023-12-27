1ac20aefa73e55427898c39e7cb5b4d44b72a5ef
Hussain Imtiaz <92842973+designbyhsn@users.noreply.github.com>
2023-12-27 04:56:13 -0800

    Email Login + Contact

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import SignUp from "./components/Signup.jsx";
+import Login from "./components/Login.jsx";
+import Protected from "./components/Protected.jsx";
+import Home from "./components/Home.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="signup" element={<SignUp />} />
+      <Route path="login" element={<Login />} />
+      <Route path="/" element={<Protected />}>
+        <Route path="/" index element={<Home />} />
+      </Route>
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
